dfc1437fbeb2c1e92774319f57a4ec86a043887d
ytyaru <yttry0@gmail.com>
2026-05-30 09:42:19 +0900

    add 5

---
+++ docs/5/src/js/main.js
@@ -0,0 +1,16 @@
+import {a} from './part/a.js';
+import {getFORL} from './part/forl.js';
+import {getId} from './part/get-id.js';
+import {typOf} from './part/typof.js';
+export class Typ {
+    static id(...v) {
+        if (v.length<1) {throw new Error(`引数不足。引数は判定対象値が必要です。`)}
+        return getId(v[0]);
+    }
+    static of(...args) {return Typ.#of(false, ...args)}
+    static throw(...args) {return Typ.#of(true, ...args)}
+    static #of(isThrow, ...args) {
+        if (args.length<2) {throw new Error(`引数不足。引数は判定対象値と期待型の二つ必要です。`)}
+        return typOf(isThrow, ...args);
+    }
+}
